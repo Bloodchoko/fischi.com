0cb7562b865d0100da13b0768ebfe70c4346784c
Ilusa Kloth <bloodchoko@me.com>
2018-05-04 22:12:19 +0200

    initial commit fischi fischi

---
+++ fischi/src/main/java/com/fischi/service/UserService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.fischi.service;
+
+import java.util.List;
+
+import com.fischi.model.UserDetails;
+
+public interface UserService {
+
+	List<UserDetails> getUserDetails();
+
+}
